98e497cb18c6cfee4ce996862611ef68037790a4
Sorokin Ivan <Spanhbob98@mail.ru>
2024-10-28 22:41:50 +0300

    вывод на странице списка разделов с элементами, с возможностью вывода для админа

---
+++ local/components/reviews.list/templates/.default/template.php
@@ -0,0 +1,21 @@
+<?php if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+
+<div class="sections-wrapper">
+    <?php foreach ($arResult['SECTIONS'] as $section): ?>
+        <div class="section">
+            <h2><?= $section['NAME'] ?></h2>
+            <?php if (!empty($section['ELEMENTS'])): ?>
+                <ul class="elements-list">
+                    <?php foreach ($section['ELEMENTS'] as $element): ?>
+                        <li>
+                            <a href="<?= $element['DETAIL_PAGE_URL'] ?>"><?= $element['NAME'] ?></a>
+                            <p><?= $element['PREVIEW_TEXT'] ?></p>
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            <?php else: ?>
+                <p>В этом разделе пока нет элементов.</p>
+            <?php endif; ?>
+        </div>
+    <?php endforeach; ?>
+</div>
