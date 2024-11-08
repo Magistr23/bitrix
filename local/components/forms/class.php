4d6170e7b9f0259e9381faa72b9ea4e4cd1fd481
Sorokin Ivan <Spanhbob98@mail.ru>
2024-11-09 00:03:57 +0300

    добавление валидации для формы

--- local/components/forms/class.php
+++ local/components/forms/class.php
@@ -13,20 +13,6 @@ class formComponent extends CBitrixComponent
 
     public function doAction()
     {
-        // Проверяем, была ли отправлена форма
-        if ($_SERVER["REQUEST_METHOD"] === "POST" && check_bitrix_sessid()) {
-            // Здесь происходит обработка данных формы
-            $formData = $_POST;
-        
-            // Выполните необходимые действия, например, сохранение данных в инфоблок или отправку на почту
-            // ...
-        
-            // Перенаправление на ту же страницу для предотвращения повторной отправки данных
-            LocalRedirect($APPLICATION->GetCurPageParam("", array("form_submitted")));
-        }
-
-
-
         if ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST['name']) && !empty($_POST['review'])) {
             // Подключаем модуль инфоблоков
             Loader::includeModule("iblock");
@@ -34,25 +20,29 @@ class formComponent extends CBitrixComponent
             // ID инфоблока, куда будут сохраняться отзывы
             $iblockId = 5; // Укажите ID вашего инфоблока
             $sectionId = 9; // Укажите ID вашего инфоблока
-        
-            // Массив данных для создания элемента
-            $arFields = array(
-                "IBLOCK_ID" => $iblockId,
-                'IBLOCK_SECTION_ID' => $sectionId,
-                "NAME" => htmlspecialchars($_POST['name']),
-                "PROPERTY_VALUES" => ["company" => htmlspecialchars($_POST['company'])],
-                "DETAIL_TEXT" => htmlspecialchars($_POST['review']),
-                "ACTIVE" => "Y",
-            );
-        
-            $el = new CIBlockElement;
-            if ($elementId = $el->Add($arFields)) {
-                echo '<div class="result">Ваш отзыв отправлен на модерацию!</div>';
-                // header("Location: http://bitrix/reviews/");
+            if (!empty($_POST['name']) && !empty($_POST['company']) && !empty($_POST['review'])) {
+                // Массив данных для создания элемента
+                $arFields = array(
+                    "IBLOCK_ID" => $iblockId,
+                    'IBLOCK_SECTION_ID' => $sectionId,
+                    "NAME" => htmlspecialchars($_POST['name']),
+                    "PROPERTY_VALUES" => ["company" => htmlspecialchars($_POST['company'])],
+                    "DETAIL_TEXT" => htmlspecialchars($_POST['review']),
+                    "ACTIVE" => "Y",
+                );
+            
+                $el = new CIBlockElement;
+                if ($elementId = $el->Add($arFields)) {
+                    echo '<div class="result">Ваш отзыв отправлен на модерацию!</div>';
+                    // header("Location: http://bitrix/reviews/");
+                } else {
+                    echo  "Ошибка отправки отзыва: " . $el->LAST_ERROR;
+                }
             } else {
-                echo  "Ошибка отправки отзыва: " . $el->LAST_ERROR;
+                echo '<div class="result">Заполните все поля</div>';
             }
         }
+        
         return true;
     }
 }
