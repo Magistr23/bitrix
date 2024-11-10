bd751ad680c2bfd3fc999eecb6be9308cccddc5e
Sorokin Ivan <Spanhbob98@mail.ru>
2024-11-10 13:03:30 +0300

    добавление проверки на странице

--- proverit/index.php
+++ proverit/index.php
@@ -1,6 +1,15 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Проверить");
+global $USER;
+
+if (!$USER->IsAuthorized()) {
+    // Проверка на наличие прав администратора
+		if (!$USER->IsAdmin()) {
+			header("Location: http://bitrix/");
+		}
+	} 
+
 ?><?$APPLICATION->IncludeComponent(
 	"reviews.list.admin", 
 	".default", 
